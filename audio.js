b43a75aaf76d307b70630538367f55806039b1d9
Shubayu <122921600+Shubayu@users.noreply.github.com>
2023-06-01 21:05:47 +0200

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,102 @@
+const audio = {
+    Map: new Howl({
+        src: "/Audio/bgm1.mp3",
+        html5: true,
+        volume: 0.005,
+        loop: true
+    }),
+
+    initBattle: new Howl({
+        src: "./Audio/initBattle.wav",
+        html5: true,
+        volume: 0.05
+    }),
+
+    battle: new Howl({
+        src: "./Audio/battle.mp3",
+        html5: true,
+        volume: 0.01
+    }),
+
+    eepHit: new Howl({
+        src: "./Audio/Sleep.wav",
+        html5: true,
+        volume: 0.8
+    }),
+
+    cryHit: new Howl({
+        src: "./Audio/BananaCry.wav",
+        html5: true,
+        volume: 0.8
+    }),
+
+    booHit: new Howl({
+        src: "./Audio/GhostMeow.wav",
+        html5: true,
+        volume: 0.8
+    }),
+
+    foiaballHit: new Howl({
+        src: "./Audio/feuerBall.wav",
+        html5: true,
+        volume: 0.7
+    }),
+
+    hapiHit: new Howl({
+        src: "./Audio/hapiSound.wav",
+        html5: true,
+        volume: 0.8
+    }),
+
+    victory: new Howl({
+        src: "./Audio/dinky.wav",
+        html5: true,
+        volume: 0.8
+    }),
+
+    soundHit: new Howl({
+        src: "./Audio/tackleHit.wav",
+        html5: true,
+        volume: 0.1
+    }),
+
+    pop: new Howl({
+        src: "./Audio/pop.wav",
+        html5: true,
+        volume: 0.3
+    }),
+
+    walk: new Howl({
+        src: "./Audio/walkSound.wav",
+        html5: true,
+        volume: 0.3
+    }),
+
+    talk: new Howl({
+        src: "./Audio/juwaSound.wav",
+        html5: true,
+        volume: 0.8
+    }),
+
+    juwaLove: new Howl({
+        src: "./Audio/juwaLove.wav",
+        html5: true,
+        volume: 0.8
+    }),
+
+    zaWarudo: new Howl({
+        src: "./Audio/zaWarudoSound.wav",
+        html5: true,
+        volume: 0.8
+    }),
+
+    strangeDevice: new Howl({
+        src: "./Audio/strangeDeviceSound.wav",
+        html5: true,
+        volume: 0.8
+    })
+
+
+
+    
+    }
